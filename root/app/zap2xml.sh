7a7ef4924f505c38ab8615533309c82500fe9f0d
jobrien2001 <jobrien2001@hotmail.com>
2019-02-24 01:58:15 -0500

    Update zap2xml.sh

--- root/app/zap2xml.sh
+++ root/app/zap2xml.sh
@@ -8,3 +8,8 @@ echo "cronjob running zap2xml on "$(date)
 . /app/zap2xml.conf
 cd /config
 /config/zap2xml.pl ${EXTRAPARAM}
+if [ ! -z ${EXTRAPARAM2+x} ]; then /config/zap2xml.pl ${EXTRAPARAM2}; fi
+if [ ! -z ${EXTRAPARAM3+x} ]; then /config/zap2xml.pl ${EXTRAPARAM3}; fi
+if [ ! -z ${EXTRAPARAM4+x} ]; then /config/zap2xml.pl ${EXTRAPARAM4}; fi
+if [ ! -z ${EXTRAPARAM5+x} ]; then /config/zap2xml.pl ${EXTRAPARAM5}; fi
+if [ "$SHUTDOWN" == "true" ]; then shutdown now; fi
